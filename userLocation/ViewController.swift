8a547272a7d148f3c848a494f5e8ef136e5376d3
Elva Wang <elva316@gmail.com>
2017-11-11 14:38:55 -0800

    first commit

--- userLocation/ViewController.swift
+++ userLocation/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // Map 
+    @IBOutlet weak var map: MKMapView!
+//    private var mapChangedFromUserInteraction = false
+
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {  // going to extract user's location
+        let location = locations[0]   // the most position of user
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)    //  how much we want user to zoomed in location
+        let myLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)   // this is the location of the user
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)    // combine, this is how much we want to current user zoom in
+        map.setRegion(region, animated: true)     //   we set on the map
+        self.map.showsUserLocation = true         //   adding the blue dot
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest     // where the user is
+        manager.requestWhenInUseAuthorization()              //  get location wehen user using the app
+        manager.startUpdatingLocation()                 //  update each time location is updated
     }
 
     override func didReceiveMemoryWarning() {
